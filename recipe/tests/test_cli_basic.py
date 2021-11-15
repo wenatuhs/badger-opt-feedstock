655e66a8eb91b99001f4923e447d9d560108cc0d
Zhe Zhang <zhezhang@slac.stanford.edu>
2021-11-15 11:48:07 -0800

    Attempt to pack tests with recipe for test-outside

---
+++ recipe/tests/test_cli_basic.py
@@ -0,0 +1,39 @@
+import os
+from pkg_resources import get_distribution
+
+
+pytest_plugins = ['pytester']
+
+
+# def test_cli_main(testdir):
+#     args = ['badger']
+#     result = testdir.run(*args)
+
+#     assert result.ret == 0
+
+#     # Check output lines
+#     outlines = result.outlines
+#     assert len(outlines) == 6
+
+#     # Check name
+#     assert outlines[0] == 'name: Badger the optimizer'
+
+#     # Check version
+#     version = get_distribution('badger-opt').version
+#     assert outlines[1] == f'version: {version}'
+
+
+# def test_cli_run(testdir, mock_config_root):
+#     args = ['badger', 'run', '-a', 'silly', '-ap',
+#             '{dimension: 2, max_iter: 10}',  '-e', 'silly', '-c',
+#             os.path.join(mock_config_root, 'test.yaml'), '-y']
+#     result = testdir.run(*args)
+
+#     assert result.ret == 0
+
+#     # Check output lines
+#     outlines = result.outlines
+#     assert len(outlines) == 14
+
+#     # Check table header
+#     assert outlines[1] == '|    iter    |     l2     |     q1     |     q2     |'
